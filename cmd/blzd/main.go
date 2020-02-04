3273bf2adb3d18295a17715aa90eec2e81d1a282
Neeraj Murarka <njmurarka@users.noreply.github.com>
2020-02-03 19:38:15 -0800

    CM-118, CM-119 - Added support for minimum gas prices, pruning, half height, and halt time (#11)
    * Added support for pruning, half height, and halt time
    
    * CM-118 and CM-119 - Done and formatted to support command line and app.toml options

--- cmd/blzd/main.go
+++ cmd/blzd/main.go
@@ -18,12 +18,16 @@ import (
 	"encoding/json"
 	"io"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/store"
 	"github.com/cosmos/cosmos-sdk/x/genaccounts"
 	genaccscli "github.com/cosmos/cosmos-sdk/x/genaccounts/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
 	"github.com/tendermint/tendermint/libs/cli"
 	"github.com/tendermint/tendermint/libs/log"
 
@@ -78,7 +82,12 @@ func main() {
 }
 
 func newApp(logger log.Logger, db dbm.DB, _ io.Writer) abci.Application {
-	return app.NewCRUDApp(logger, db)
+	return app.NewCRUDApp(logger,
+		db,
+		baseapp.SetMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
+		baseapp.SetPruning(store.NewPruningOptionsFromString(viper.GetString("pruning"))),
+		baseapp.SetHaltHeight(viper.GetUint64(server.FlagHaltHeight)),
+		baseapp.SetHaltTime(viper.GetUint64(server.FlagHaltTime)))
 }
 
 func exportAppStateAndTMValidators(
