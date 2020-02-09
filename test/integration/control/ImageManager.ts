02b2c1623712824159421eba0b63c9063baceedb
scott <scott@bulldoginfo.com>
2020-02-08 16:23:08 -0800

    manager stuff coming along

--- test/integration/control/ImageManager.ts
+++ test/integration/control/ImageManager.ts
@@ -1,9 +1,18 @@
 import {Image} from "node-docker-api/lib/image";
 import {Docker} from "node-docker-api";
 import {DockerImage} from "./DockerImage";
+import {Stream} from "stream";
+//import {Stream} from "stream";
+
+const tar = require('tar-fs');
 
 const docker = new Docker({socketPath: '/var/run/docker.sock'});
 
+const promisifyStream = (stream: Stream): Promise<Stream> => new Promise((resolve, reject) => {
+    stream.on('data', data => console.log(data.toString()));
+    stream.on('end', resolve);
+    stream.on('error', reject);
+});
 
 export const listImages = (prefix: string): Promise<DockerImage[]> => {
     const filterTestImages = (prefix: string) => (it: Image): Image[] => (it.data as any).RepoTags
@@ -13,4 +22,14 @@ export const listImages = (prefix: string): Promise<DockerImage[]> => {
     return docker.image.list()
         .then((images: Image[]): Image[] => images.filter(filterTestImages(prefix)))
         .then((images: Image[]) => images.map<DockerImage>((image: Image): DockerImage => new DockerImage(image)));
+};
+
+export const createImageFromFile = (prefix:string, name:string): Promise<DockerImage> => {
+    var tarStream = tar.pack(`${__dirname}/dockerfiles`);
+    return (docker.image.build(tarStream, {
+        t: `${prefix}/${name}`
+    }) as Promise<Stream>)
+        .then((stream: Stream) => promisifyStream(stream))
+        .then(() => docker.image.get(`${prefix}/${name}`).status())
+        .then(image => new DockerImage(image))
 };
--- test/integration/control/manager.ts
+++ test/integration/control/manager.ts
@@ -1,4 +1,24 @@
-import {listImages} from "./ImageManager";
+//import {createImageFromFile, listImages} from "./ImageManager";
+import {createImageFromFile, listImages} from "./ImageManager";
+import {DockerImage} from "./DockerImage";
+
+export const listDaemonImages = (): Promise<DockerImage[]> =>
+    listImages('integration');
+
+const ensureBaseImage = (): Promise<boolean> =>
+    listImages('integration')
+        .then(images => !!images.find(image => image.shortName === 'base-image'))
+        .then(result => result ? result : !!createImageFromFile('integration', 'base-image'));
 
 
-listImages('test').then(console.log);
+export const startDaemon = async (name: string): Promise<boolean> => {
+    await ensureBaseImage();
+
+    return new Promise(() => {});
+};
+
+startDaemon('testing');
+
+//createImageFromFile('integration', 'base-image');
+
+//listImages('test').then(console.log);
