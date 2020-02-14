37e814d755cea9394f19050dc4554caa2cbe3506
scott <scott@bulldoginfo.com>
2020-02-14 11:20:14 -0800

    added typescript definition file

---
+++ test/integration/scripts/blz-tmclient/lib/bluzelle-node.d.ts
@@ -0,0 +1,25 @@
+interface BluzelleConfig {
+    address: string,
+    mnemonic: string,
+    endpoint: string,
+    chain_id: string
+}
+
+interface GasInfo {
+    gas_price: string
+}
+
+interface Result {
+    UUID: string,
+    Key: string,
+    Value: string
+}
+
+declare class API {
+    create: (key: string, value: string, gasInfo: GasInfo) => Promise<Result>;
+    has: (key: string, gasInfo: GasInfo) => Promise<Result>;
+    quickread: (key: string) => Promise<Result>;
+}
+
+export function bluzelle(config: BluzelleConfig): Promise<API>
+
