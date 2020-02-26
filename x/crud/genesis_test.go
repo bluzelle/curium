c7cd285ebf04916e6b439d7476f2f4e1e14588c6
rnistuk <rnistuk@gmail.com>
2020-02-25 16:02:03 -0800

    CM-153 Added genesis tests

--- x/crud/genesis_test.go
+++ x/crud/genesis_test.go
@@ -2,14 +2,28 @@ package crud
 
 import (
 	"github.com/bluzelle/curium/x/crud/internal/types"
+	"github.com/bluzelle/curium/x/crud/mocks"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/cachekv"
+	"github.com/cosmos/cosmos-sdk/store/dbadapter"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	abci "github.com/tendermint/tendermint/abci/types"
+	dbm "github.com/tendermint/tm-db"
 	"testing"
 )
 
+func initKeeperTest(t *testing.T) (sdk.Context, sdk.KVStore, []byte, *codec.Codec) {
+
+	return sdk.NewContext(nil, abci.Header{}, false, nil),
+		cachekv.NewStore(dbadapter.Store{dbm.NewMemDB()}),
+		[]byte("bluzelle1t0ywtmrduldf6h4wqrnnpyp9wr6law2u5jwa23"),
+		codec.New()
+}
+
 func TestNewGenesisState(t *testing.T) {
-	sut := NewGenesisState(nil)
-	if len(sut.BlzValues) > 0 {
-		t.Error("Expected empty BlzValues in new genesis state")
-	}
+	assert.Empty(t, NewGenesisState(nil).BlzValues)
 }
 
 func TestValidateGenesis(t *testing.T) {
@@ -17,43 +31,31 @@ func TestValidateGenesis(t *testing.T) {
 
 	genesisState := NewGenesisState(blzValues)
 
-	err := ValidateGenesis(genesisState)
+	assert.Nil(t, ValidateGenesis(genesisState))
 
-	if err != nil {
-		t.Error("Expected nil error")
-	}
+	blzValues = append(blzValues, types.BLZValue{"test", []byte("notnilowner")})
 
-	blzValues = append(blzValues, types.BLZValue{
-		"test", []byte("notnilowner"),
-	})
+	assert.Nil(t, ValidateGenesis(genesisState))
 
-	err = ValidateGenesis(genesisState)
+	blzValues = append(blzValues, types.BLZValue{"test", nil})
 
-	if err != nil {
-		t.Error("Expected error due to empty owner")
-	}
-
-	blzValues = append(blzValues, types.BLZValue{
-		"test",
-		nil,
-	})
-
-	err = ValidateGenesis(genesisState)
-
-	if err != nil {
-		t.Error("Expected error due to empty owner")
-	}
-
-}
-
-func TestDefaultGenesisState(t *testing.T) {
-
+	assert.Nil(t, ValidateGenesis(genesisState))
 }
 
 func TestInitGenesis(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	mockKeeper := mocks.NewMockIKeeper(mockCtrl)
+	data := DefaultGenesisState()
+	ctx := sdk.Context{}
+	owner := []byte("bluzelle1t0ywtmrduldf6h4wqrnnpyp9wr6law2u5jwa23")
+	data.BlzValues = append(data.BlzValues, types.BLZValue{Value: "test", Owner: owner})
 
-}
+	mockKeeper.EXPECT().
+		SetBLZValue(ctx, nil, "UUID-Genesis", "Key-Genesis",
+			types.BLZValue{Value: "test", Owner: owner})
 
-func TestExportGenesis(t *testing.T) {
+	mockKeeper.EXPECT().
+		GetKVStore(ctx).Return(nil)
 
+	InitGenesis(ctx, mockKeeper, data)
 }
