acf7ab7c7911efca7c4da3bf51d73e2cfbf9d51b
Rich Nistuk <rnistuk@users.noreply.github.com>
2020-02-26 16:24:21 -0800

    CM-157 implemented msgs.go unit tests (#43)

--- x/crud/internal/types/types_test.go
+++ x/crud/internal/types/types_test.go
@@ -1,3 +1,17 @@
+// Copyright (C) 2020 Bluzelle
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License, version 3,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 package types
 
 import (
@@ -36,5 +50,4 @@ func TestBLZValue_String(t *testing.T) {
 		Value: "value",
 	}
 	assert.Equal(t, value.String(), "Value: value Owner: <empty>")
-
 }
