b600fa2429bbae99159c0aceff76d28fa33dd5c7
Matt Ilagan <matt.r.ilagan@gmail.com>
2020-03-01 00:27:16 -0800

    Fix network interface in config toml

--- docker/blz-setup.sh
+++ docker/blz-setup.sh
@@ -2,6 +2,7 @@
 
 if [ "$NODE_NAME" = "swarm01" ]; then
   tar xvf ./blz-test.tar.xz
+  sed -i -e 's/127.0.0.1:26657/0.0.0.0:26657/g' .blzd/config/config.toml
   blzd start &
   sleep 5s
   blzcli rest-server --laddr tcp://0.0.0.0:1317
@@ -12,6 +13,7 @@ else
   blzcli config indent true
   blzcli config trust-node true
   tar -xf blz-test.tar.xz .blzd/config/genesis.json
+  sed -i -e 's/127.0.0.1:26657/0.0.0.0:26657/g' .blzd/config/config.toml
   sed -i -e 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01bnt"/g' .blzd/config/app.toml
   NODE_ID=$(cat .blzd/config/genesis.json | grep \"memo\" | awk -F'[@|:|\"]' '{print $5}')@${LOCAL_IP}:26656
   blzd start --p2p.persistent_peers ${NODE_ID}
