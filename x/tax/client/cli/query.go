cc98440fd6ec3bce40ff7d0571512e5b70ce15f7
junkai121 <junkai121@outlook.com>
2020-10-29 02:14:04 +1000

    fix query tax info

--- x/tax/client/cli/query.go
+++ x/tax/client/cli/query.go
@@ -11,12 +11,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// GetQueryCmd returns module query commands
 func GetQueryCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 	taxQueryCmd := &cobra.Command{
 		Use:                        types.ModuleName,
 		Short:                      "Querying commands for the tax module",
 		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
+		SuggestionsMinimumDistance: 0,
 		RunE:                       client.ValidateCmd,
 	}
 
@@ -27,19 +28,18 @@ func GetQueryCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 	return taxQueryCmd
 }
 
+// GetCmdQTaxInfo returns tax info by query
 func GetCmdQTaxInfo(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
 		Use:   "info",
 		Short: "show tax info",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
-			UUID := args[0]
-			key := args[1]
-			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/owner/%s/%s", queryRoute, UUID, key), nil)
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/tax_info", queryRoute), nil)
 
 			if err != nil {
-				fmt.Printf("could get owner for key - %s : %s\n", UUID, key)
+				fmt.Println("could get tax info ", err)
 				return nil
 			}
 
