e6f54507598a8e3088239be62ae55d8c3f8aebe3
scott <scott@bulldoginfo.com>
2020-12-31 23:48:17 -0700

    skeleton app there

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang/mock v1.4.0
 	github.com/gorilla/mux v1.7.4
 	github.com/magiconair/properties v1.8.1
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.6.3
 	github.com/stretchr/testify v1.6.1
--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -1,6 +1,8 @@
 package oracle
 
 import (
+	"fmt"
+	"github.com/bluzelle/curium/x/crud"
 	"github.com/bluzelle/curium/x/oracle/keeper"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -8,11 +10,12 @@ import (
 
 // BeginBlocker check for infraction evidence or downtime of validators
 // on every begin block
-func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) {
-// 	TODO: fill out if your application requires beginblock, if not you can delete this function
+func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper, crudKeeper crud.Keeper) {
 }
 
 // EndBlocker called every block, process inflation, update validator set.
-func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
-// 	TODO: fill out if your application requires endblock, if not you can delete this function
+func EndBlocker(ctx sdk.Context, _ keeper.Keeper, crudKeeper crud.Keeper) {
+	keys := crudKeeper.GetKeys(ctx, crudKeeper.GetKVStore(ctx), "oracle", nil)
+	fmt.Println("**********************************")
+	fmt.Println(keys)
 }
