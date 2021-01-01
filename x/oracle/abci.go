cec9cbbd9fedf00f239d78902d2cf42e99609cf3
scott <scott@bulldoginfo.com>
2021-01-01 06:52:51 -0700

    feeder starting

--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -1,7 +1,6 @@
 package oracle
 
 import (
-	"fmt"
 	"github.com/bluzelle/curium/x/crud"
 	"github.com/bluzelle/curium/x/oracle/keeper"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,11 +10,9 @@ import (
 // BeginBlocker check for infraction evidence or downtime of validators
 // on every begin block
 func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper, crudKeeper crud.Keeper) {
+	currCtx = &ctx
 }
 
 // EndBlocker called every block, process inflation, update validator set.
 func EndBlocker(ctx sdk.Context, _ keeper.Keeper, crudKeeper crud.Keeper) {
-	keys := crudKeeper.GetKeys(ctx, crudKeeper.GetKVStore(ctx), "oracle", nil)
-	fmt.Println("**********************************")
-	fmt.Println(keys)
 }
