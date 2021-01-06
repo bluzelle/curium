6b8fa94a78b30000beb1969982f1a37450293fbb
scott <scott@bulldoginfo.com>
2021-01-06 13:19:07 -0700

    Added list-sources

--- x/oracle/types/types.go
+++ x/oracle/types/types.go
@@ -3,9 +3,9 @@ package types
 import "github.com/cosmos/cosmos-sdk/types"
 
 type Source struct {
-	Name     string
+	Name     string `json:"name"`
 	Url      string `json:"url"`
 	Property string `json:"property"`
-	Owner	types.AccAddress
+	Owner	types.AccAddress `json:"owner"`
 }
 
