4036c8ca3d894983383204e966b72626a4544651
scott <scott@bulldoginfo.com>
2021-01-21 01:45:30 -0700

    added query endpoint to oracle

---
+++ test/integration/specs/oracle/source-utils.ts
@@ -0,0 +1,16 @@
+import {API} from "../../../../../blzjs/client";
+
+export const deleteSources = (bz: API) =>
+    bz.abciQuery<any>('oracle/query', 'custom/oracle/listsources')
+        .then((sources: any) => bz.withTransaction(() =>
+                Promise.all(sources.result.map(source => deleteSource(bz, source.name)))
+            )
+        )
+const deleteSource = (bz: API, name: string): Promise<any> =>
+    bz.sendMessage({
+        type: 'oracle/deletesource',
+        value: {
+            Name: name,
+            Owner: bz.address
+        }
+    }, {gas_price: 0.002, max_gas: 10000000});
