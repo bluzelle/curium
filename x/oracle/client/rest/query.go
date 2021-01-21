dd55a0ddb9797b13befdc8167df5b4de6cc19a80
scott <scott@bulldoginfo.com>
2021-01-21 15:05:59 -0700

    removed unnecessary rest code

--- x/oracle/client/rest/query.go
+++ x/oracle/client/rest/query.go
@@ -1,65 +1,13 @@
 package rest
 
 import (
-	"encoding/hex"
-	"io/ioutil"
-	"net/http"
-
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 )
 
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc(
-		"/oracle/query",
-		abciQueryHandlerFn(cliCtx),
-		).Methods("POST")
 }
-
-type queryReq struct {
-	Path string
-	Data string
-}
-
-
-func abciQueryHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
-		if !ok {
-			return
-		}
-
-		var req queryReq
-
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-		}
-
-		err = cliCtx.Codec.UnmarshalJSON(body, &req)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-		}
-
-		bytes, err := hex.DecodeString(req.Data)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-		}
-		res, height, err := cliCtx.QueryWithData(req.Path, bytes)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-		}
-
-		cliCtx = cliCtx.WithHeight(height)
-		rest.PostProcessResponse(w, cliCtx, res)
-	}
-}
-
-
-
 
 
 
