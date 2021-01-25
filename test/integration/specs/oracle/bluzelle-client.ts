59d1116a81f1b3bde49c8e4bc78bb1b46bff8900
scott <scott@bulldoginfo.com>
2021-01-24 18:39:09 -0700

    now checks if a vote is good

--- test/integration/specs/oracle/bluzelle-client.ts
+++ test/integration/specs/oracle/bluzelle-client.ts
@@ -1,10 +1,11 @@
-import {bluzelle} from "../../../../../blzjs/client";
+import {bluzelle, BluzelleConfig} from "bluzelle";
 import {memoize} from 'lodash'
 
-export const getBzClient = memoize(() =>
+export const getBzClient = memoize((config: Partial<BluzelleConfig> = {}) =>
     bluzelle({
         mnemonic: 'bone soup garage safe hotel remove rebuild tumble usage marriage skin opinion banana scene focus obtain very soap vocal print symptom winter update hundred',
         endpoint: 'http://localhost:1317',
-        uuid: 'uuid'
+        uuid: 'uuid',
+        ...config
     })
 );
