9edc0644ace2c13c14d5d4a400f321263f7894ac
scott <scott@bulldoginfo.com>
2021-01-28 01:51:03 -0700

    moved fee calculation to a function in gasFeeCalculator

--- app/ante/NewDeductFeeDecorator.go
+++ app/ante/NewDeductFeeDecorator.go
@@ -39,20 +39,7 @@ func NewDeductFeeDecorator(ak keeper.AccountKeeper, sk types.SupplyKeeper) Deduc
 }
 
 func (dfd DeductFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	var gasPrices sdk.Dec
-	gasPrices = ctx.MinGasPrices().AmountOf("ubnt")
-	// If I can't get the gas price from the context, get it from the transaction
-	if gasPrices.IsZero() {
-		stdTx := tx.(types.StdTx)
-		fee := stdTx.GetFee().AmountOf("ubnt").Int64()
-		gas := stdTx.GetGas()
-		gasPrice := float64(fee) / float64(gas)
-		gasPrices, _ = sdk.NewDecFromStr(fmt.Sprintf("%f", gasPrice))
-	}
-
-	gasConsumed := ctx.GasMeter().GasConsumed()
-	gasFee := gasPrices.MulInt64(int64(gasConsumed)).RoundInt64()
-	gasFeeCoins := sdk.NewCoins(sdk.NewCoin("ubnt", sdk.NewInt(gasFee)))
+	gasFeeCoins := CalculateGasFee(tx, ctx)
 
 	feeTx, ok := tx.(FeeTx)
 	if !ok {
