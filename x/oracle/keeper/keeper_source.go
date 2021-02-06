e9534f983f490f41aeb9858b46ab6eff97fbd225
scott <scott@bulldoginfo.com>
2021-02-06 16:25:31 -0700

    removed test code

--- x/oracle/keeper/keeper_source.go
+++ x/oracle/keeper/keeper_source.go
@@ -36,8 +36,6 @@ func (k Keeper) ListSources(ctx types.Context) ([]types2.Source, error) {
 		var source types2.Source
 		value := iterator.Value()
 		k.cdc.UnmarshalBinaryLengthPrefixed(value, &source)
-		key := string(iterator.Key())
-		_ = key
 		sources = append(sources, source)
 	}
 	return sources, nil
