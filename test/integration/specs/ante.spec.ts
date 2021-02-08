b3ab19d51b18466a1886b2c442ee54573c1d8f1c
scott <scott@bulldoginfo.com>
2021-02-07 19:12:30 -0700

    fixed test

--- test/integration/specs/ante.spec.ts
+++ test/integration/specs/ante.spec.ts
@@ -2,38 +2,35 @@ import {BLOCK, blzcli, STANDARD_GAS} from "../helpers/blzcli";
 import {getBzClient} from "./oracle/bluzelle-client";
 import {passThroughAwait} from "promise-passthrough";
 import {expect} from 'chai'
+import {addSource} from "./oracle/oracle-utils";
 
 describe('Ante handler tests', () => {
     it("should charge for regular transactions", async () => {
         await shouldCharge(
-            () => blzcli('tx send vuser bluzelle1weav4c4j6nyxa5kggghhf7ymu4x0w7caaggcda 1000ubnt -y', STANDARD_GAS, BLOCK),
-            x => x - 1000
-        )
-        await shouldCharge(
-            () => blzcli('tx crud upsert uuid foo bar -y --from vuser', STANDARD_GAS, BLOCK)
+            (bz) => bz.upsert('foo', 'bar', {gas_price: 0.002, max_gas: 10000000})
         )
     });
 
     it('should not charge for oracle transactions', async () => {
         await shouldBeFree(
-            () => blzcli('tx oracle add-source aaa bbb ccc -y --from vuser', STANDARD_GAS, BLOCK)
+            (bz) => addSource(bz, 'my-source', 'my-url', 'my-property', bz.address)
         )
     });
 
 })
 
 type CheckFn = (number) => number
-const shouldCharge = (fn: () => Promise<unknown>, checkFn: CheckFn = (x) => x) =>
+const shouldCharge = (fn: (API) => Promise<unknown>, checkFn: CheckFn = (x) => x) =>
     getBzClient().getBNT({ubnt: true})
-        .then(passThroughAwait(fn))
+        .then(passThroughAwait(() => fn(getBzClient())))
         .then(async (before) =>
             expect(await getBzClient().getBNT({ubnt: true}))
                 .to.be.lessThan(checkFn(before))
         )
 
-const shouldBeFree = (fn: () => Promise<unknown>) =>
+const shouldBeFree = (fn: (API) => Promise<unknown>) =>
     getBzClient().getBNT({ubnt: true})
-        .then(passThroughAwait(fn))
+        .then(passThroughAwait(() => fn(getBzClient())))
         .then(async (before) =>
             expect(await getBzClient().getBNT({ubnt: true}))
                 .to.equal(before)
