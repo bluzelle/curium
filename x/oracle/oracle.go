2f677f937949b59ac7751137e287d4f89d08d93f
scott <scott@bulldoginfo.com>
2021-02-15 07:44:15 -0700

    fixed tight loop

--- x/oracle/oracle.go
+++ x/oracle/oracle.go
@@ -26,7 +26,7 @@ func Run(oracleKeeper Keeper, accountKeeper auth.AccountKeeper, cdc *codec.Codec
 
 func waitForCtx() {
 	for currCtx == nil {
-		time.After(time.Second)
+		time.Sleep(time.Second)
 	}
 }
 
