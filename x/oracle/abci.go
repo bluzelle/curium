95509cdb96a990bd9eb3bd514abf78c203d3d2dd
scott <scott@bulldoginfo.com>
2021-03-10 15:20:56 -0700

    change contexts to use dummy gas meter when started by the beginning/end blocker

--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -1,15 +1,16 @@
 package oracle
 
 import (
+	"github.com/bluzelle/curium/app/ante"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
-	"time"
 )
 
 // BeginBlocker check for infraction evidence or downtime of validators
 // on every begin block
 func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) {
-	time.AfterFunc(10 * time.Second, func() {currCtx = &ctx})
+	ctx = ctx.WithGasMeter(ante.NewDummyGasMeter())
+	currCtx = &ctx
 }
 
 // EndBlocker called every block, process inflation, update validator set.
