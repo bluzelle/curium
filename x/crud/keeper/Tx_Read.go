78159c6697215a80d6a83eff89f15470b1999b21
avendauz <aven@bluzelle.com>
2021-06-12 14:15:07 -0700

    variable for getting value, remove hasCrudValue check

--- x/crud/keeper/Tx_Read.go
+++ x/crud/keeper/Tx_Read.go
@@ -2,9 +2,6 @@ package keeper
 
 import (
 	"context"
-	"fmt"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
 	"github.com/bluzelle/curium/x/crud/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -12,10 +9,13 @@ import (
 func (k msgServer) Read(goCtx context.Context, msg *types.MsgRead) (*types.MsgReadResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	if !k.HasCrudValue(&ctx, msg.Uuid, msg.Key) {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %s doesn't exist", msg.Key))
-	}
-
-	return &types.MsgReadResponse{}, nil
+	//if !k.HasCrudValue(&ctx, msg.Uuid, msg.Key) {
+	//	return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %s doesn't exist", msg.Key))
+	//}
+	val := k.GetCrudValue(&ctx, msg.Uuid, msg.Key)
+	return &types.MsgReadResponse{
+		Value: val.Value,
+		Key:   msg.Key,
+	}, nil
 
 }
