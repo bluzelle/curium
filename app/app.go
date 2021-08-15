6add7675684eb41cd68e5e3931241f588783b51d
Aven Dauz <avendauz@Avens-MBP.lan>
2021-08-15 16:27:00 -0400

    added oracle module to endblocker in app.go

--- app/app.go
+++ app/app.go
@@ -448,7 +448,7 @@ func NewCRUDApp(
 	)
 
 	app.mm.SetOrderBeginBlockers(distr.ModuleName, slashing.ModuleName, oracle.ModuleName)
-	app.mm.SetOrderEndBlockers(gov.ModuleName, staking.ModuleName, aggregator.ModuleName, nft.ModuleName, curium.ModuleName, tax.ModuleName)
+	app.mm.SetOrderEndBlockers(gov.ModuleName, staking.ModuleName, aggregator.ModuleName, nft.ModuleName, curium.ModuleName, tax.ModuleName, oracle.ModuleName)
 
 	// Sets the order of Genesis - Order matters, genutil is to always come last
 	// NOTE: The genutils moodule must occur after staking so that pools are
