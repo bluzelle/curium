c1ee1ac625f614367a8bf99dbb2b0025e454d53a
Scott Burch <scott@bulldoginfo.com>
2021-08-31 20:20:03 -0700

    small demo.ts file update

--- x/nft/demo/demo.ts
+++ x/nft/demo/demo.ts
@@ -1,14 +1,16 @@
 import {bluzelle, uploadNft} from "bluzelle";
 import {sha256} from "js-sha256";
 
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
+
 Promise.resolve({
     bz: bluzelle({
-        mnemonic: '',
-        endpoint: 'http://localhost:1327',
+        mnemonic: 'document found seminar giant wet surprise unhappy detail pilot waste spot duty cage bonus moon pass pool man library zero sister chimney flee river',
+        endpoint: 'https://54.251.58.142:1317',
         uuid: ''
     }),
-    data: new TextEncoder().encode('some nft data'),
-    id: 'any-id'
+    data: new TextEncoder().encode('some nft data here - 3'),
+    id: 'any-id-3'
 })
     .then(ctx => ({...ctx, hash: sha256(ctx.data)}))
     .then(ctx =>
@@ -17,12 +19,16 @@ Promise.resolve({
             hash: ctx.hash,
             vendor: 'customer',
             userId: 'user-id',
-            mime: 'text/txt',
+            mime: 'text/plain',
             meta: 'meta',
             size: ctx.data.byteLength,
             gasInfo: {max_gas: 100_000_000, gas_price: 0.002}
         })
             .then(resp => ({...ctx, token: resp.token}))
     )
-    .then(ctx => uploadNft(ctx.bz.url, ctx.data, ctx.token, 'customer'))
+    .then(
+        ctx => uploadNft(ctx.bz.url, ctx.data, ctx.token, 'customer')
+            .then(() => ctx)
+    ).then(ctx => console.log('HASH:',ctx.hash))
+    .catch(e => console.log('ERROR:', e))
 
