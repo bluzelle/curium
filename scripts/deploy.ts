503978ee99f2bf90059be46efecf8cc63a7e57b4
matteyu <matt.r.ilagan@gmail.com>
2021-09-08 14:50:20 -0700

    Fix curium deploy script to remove deploy script on deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,11 +6,11 @@ const deployCurium = () => {
     .then(() => $`git remote add temporary-remote-curium git@github.com-bluzelle:bluzelle/test-deploy-repo.git`)
     .then(() => $`git checkout -b deploy-curium`)
     .then(() => cd('../../'))
+    .then(() => $`git filter-branch -f --tree-filter 'rm -rf curium/scripts/deploy.ts' HEAD`)
     .then(() => $`git filter-branch -f \
     --subdirectory-filter curium/ \
     --prune-empty \
     --tag-name-filter cat -- --all`)
-    .then(() => $`ls && git filter-branch -f --tree-filter 'rm -rf scripts/deploy.ts' HEAD`)
     .then(() => $`git push -f temporary-remote-curium devel`)
     .then(() => $`git fetch`)
     .then(() => $`git checkout origin/devel`)
